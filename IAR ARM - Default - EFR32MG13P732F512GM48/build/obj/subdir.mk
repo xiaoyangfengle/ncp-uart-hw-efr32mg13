026c3f2f3e4222bde7150fb2659b9ffd4ab9af35
sven.yang <yxf_on@163.com>
2019-03-16 01:29:03 +0800

    first commit

---
+++ IAR ARM - Default - EFR32MG13P732F512GM48/build/obj/subdir.mk
@@ -0,0 +1,80 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../build/obj/SEGGER_RTT.o \
+../build/obj/adc-efr32.o \
+../build/obj/antenna-stub.o \
+../build/obj/assert-crash-handlers.o \
+../build/obj/bootloader-interface-standalone.o \
+../build/obj/bootloader-interface.o \
+../build/obj/btl_interface.o \
+../build/obj/button-stub.o \
+../build/obj/buzzer-stub.o \
+../build/obj/coexistence-802154.o \
+../build/obj/coexistence-hal.o \
+../build/obj/coexistence.o \
+../build/obj/com.o \
+../build/obj/command-handlers-secure-ezsp-stub.o \
+../build/obj/crc.o \
+../build/obj/cstartup-common.o \
+../build/obj/debug-jtag-efr32.o \
+../build/obj/diagnostic.o \
+../build/obj/dmadrv.o \
+../build/obj/em260-common.o \
+../build/obj/em_adc.o \
+../build/obj/em_cmu.o \
+../build/obj/em_core.o \
+../build/obj/em_emu.o \
+../build/obj/em_gpio.o \
+../build/obj/em_i2c.o \
+../build/obj/em_ldma.o \
+../build/obj/em_leuart.o \
+../build/obj/em_mpu.o \
+../build/obj/em_msc.o \
+../build/obj/em_prs.o \
+../build/obj/em_rmu.o \
+../build/obj/em_rtcc.o \
+../build/obj/em_system.o \
+../build/obj/em_timer.o \
+../build/obj/em_usart.o \
+../build/obj/em_wdog.o \
+../build/obj/ember-configuration.o \
+../build/obj/ember-printf.o \
+../build/obj/endian.o \
+../build/obj/ezsp-command-handler.o \
+../build/obj/faults-v7m.o \
+../build/obj/gpiointerrupt.o \
+../build/obj/hal-config-gpio.o \
+../build/obj/hal-config.o \
+../build/obj/isr-stubs.o \
+../build/obj/led.o \
+../build/obj/mem-util.o \
+../build/obj/mfg-token.o \
+../build/obj/micro-common.o \
+../build/obj/micro.o \
+../build/obj/mpu.o \
+../build/obj/ncp-bookkeeping.o \
+../build/obj/ncp-callback-stubs.o \
+../build/obj/ncp-callbacks.o \
+../build/obj/ncp-events.o \
+../build/obj/random.o \
+../build/obj/rtcdriver.o \
+../build/obj/secure-ezsp-stub.o \
+../build/obj/serial-interface-uart.o \
+../build/obj/serial.o \
+../build/obj/sim-eeprom2.o \
+../build/obj/sleep-efm32.o \
+../build/obj/sleep.o \
+../build/obj/system_efr32mg13p.o \
+../build/obj/tempdrv.o \
+../build/obj/token-def.o \
+../build/obj/token.o \
+../build/obj/uartdrv.o \
+../build/obj/ustimer.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
